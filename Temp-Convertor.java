f4be43b2c78ddc16a0958103cfabc24ee036f47a
ashishharidas <ashish101111@gmail.com>
2025-11-29 00:48:46 +0530

    29/11/25

--- Temp-Convertor.java
+++ Temp-Convertor.java
@@ -3,17 +3,20 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Welcome to Temperature Converter");
         
-        while(1){
+        while(true){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Choose from the option below:");
         System.out.println("1. Convert Celsius to Fahrenheit");
         System.out.println("2. Convert Fahrenheit to Celsius");
-        int choice = scanner.nextInt();
+        System.out.println("3. Exit");
+        int choice = sc.nextInt();
         if(choice == 1){
             convertCtoF();
         } else if(choice == 2){
             convertFtoC();
-        } else {
-            System.out.println("Invalid choice. Please try again.");
+        } else if(choice == 3){
+            System.out.println("Exiting the program. ");
+            break;
 
         }
         }
@@ -22,20 +25,20 @@ class Main{
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter temperature in Celsius: ");
-        double celsius = scanner.nextDouble();
+        double celsius = sc.nextDouble();
         double fahrenheit = (celsius * 9/5) + 32;
         System.out.printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
-        scanner.close();
+       
      
     }
     public static void convertCtoF() {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter temperature in Fahrenheit: ");
-        double fahrenheit = scanner.nextDouble();
+        double fahrenheit = sc.nextDouble();
         double celsius = (fahrenheit - 32) * 5/9;
         System.out.printf("Temperature in Celsius: %.2f\n", celsius);
-        scanner.close();
+       
      
     }
 }
